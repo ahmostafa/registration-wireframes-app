74afd0438c3a34c5e930cd0aeb9270c6d03ad917
Ahmed Mostafa <engahmed.mostafa@yahoo.com>
2018-10-14 22:29:26 +0300

    create regex validation class which return validation Fn

---
+++ src/app/validation/custom-validation.ts
@@ -0,0 +1,15 @@
+import {  ValidationErrors, ValidatorFn, AbstractControl } from '@angular/forms';
+export class CustomValidation {
+
+    static regexValidation (regex: RegExp , error: ValidationErrors): ValidatorFn {
+        return (control: AbstractControl): { [key: string]: any } => {
+          if (!control.value) {
+            return null;
+          }
+          const valid = regex.test(control.value);
+          return valid ? null : error;
+         } ;
+
+      }
+
+}
